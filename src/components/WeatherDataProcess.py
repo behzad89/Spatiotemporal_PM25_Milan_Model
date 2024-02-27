efb63a44aded7184c10983f1390c5bdd6bd3d367
BVSh <b.valipour.sh@gmail.com>
2024-02-27 15:37:22 +0100

    Develop Weather Data Pipeline

--- src/components/WeatherDataProcess.py
+++ src/components/WeatherDataProcess.py
@@ -4,83 +4,83 @@ import time
 from typing import List
 
 import xarray as xr
+import pystac_client
+import planetary_computer
 from dask.distributed import LocalCluster
 from dataclasses import dataclass, field
 
 from src.exception import CustomException
 from src.logger import logging
-from src.utils.climatology import download_ERA_Land
-
 
 
 @dataclass
 class WeatherDataConfig:
-    year: int 
-    month: int
-    area: List[float] = field(default_factory=list)
-    
-    filename: str = field(init=False)
-    weather_data_path: str = field(init=False)
-    variable: List[str] = field(default_factory=lambda: ["2m_temperature"])
+    client = "https://planetarycomputer.microsoft.com/api/stac/v1/"
+    collection = ["era5-pds"]
 
-    def __post_init__(self):
-        self.filename = f"era5-land-{self.variable[0]}-{self.year:04d}-{self.month:02d}.nc"
-        self.weather_data_path = os.path.join("weather-data", self.filename)
+class WeatherDataProcessor:
+    def __init__(self,year:int, area: List[float]):
+        self.config = WeatherDataConfig
+        self.year = year
+        self.area = area
 
-class WeatherDataDownloader:
-    def __init__(self,config:WeatherDataConfig):
-        self.config = config
-
-    def download(self) -> str:
+    def Transformer(self) -> str:
         try:
-            logging.info(f"Start to download Variable -> {self.config.variable[0]}; Date ->{self.config.year}-{self.config.month}; AOI -> North:{self.config.area[0]}, West:{self.config.area[1]}, South:{self.config.area[2]}, East:{self.config.area[3]}")
-            self.data_path = download_ERA_Land(self.config.variable, 
-                            self.config.year,
-                            self.config.month,
-                            self.config.area,
-                            self.config.weather_data_path)
-            return self.data_path
-        except Exception as e:
-            raise CustomException(e,sys)
-        
-        
-class WeatherTransformer:
-    def __init__(self,files: List[str],variable:str) -> None:
-        self.files = files
-        self.variable = variable
+            logging.info(f"Start to download weather; "
+                        f"Year -> {self.year}; "
+                        f"AOI -> North:{self.area[0]}, "
+                        f"West:{self.area[1]}, "
+                        f"South:{self.area[2]}, "
+                        f"East:{self.area[3]}")
+            
+            time_range = f"{self.year}-01/{self.year}-12"
+            catalog = pystac_client.Client.open(self.config.client)
 
-    def convertor(self):
-        try:
+            search = catalog.search(collections=[self.config.collection], 
+                                    datetime=time_range,bbox=self.area)
+            
+            items = search.item_collection()
 
+            logging.info(f" The number of the items -> {len(items)}")
 
-            # Step 1: Create a Dask Cluster
-            logging.info("Step 1: Create Dask Cluster")
-            cluster = LocalCluster(n_workers=8, threads_per_worker=3)
+            datasets=[]
+            for item in items:
+                signed_item = planetary_computer.sign(item)
+                for asset in signed_item.assets.values():
+                    datasets.append(xr.open_dataset(asset.href, **asset.extra_fields["xarray:open_kwargs"]))
+
+            ds = xr.combine_by_coords(datasets, join="exact")
+            logging.info(f" The DASK object -> {ds.dims}")
+
+            # Step 1: Create a directory to save independent variables
+            logging.info("Step 1: Make a directory to save independent variables")
+            output_dir = os.path.join(os.getcwd(), "independent-variables", "weather_data")
+            os.makedirs(output_dir, exist_ok=True)
+
+            # Step 2: Create a Dask Cluster
+            logging.info("Step 2: Create Dask Cluster")
+            cluster = LocalCluster(n_workers=40, threads_per_worker=3)
             client = cluster.get_client()
             time.sleep(3)
 
-            # Step 2: Resample hourly data to daily data using xarray
-            logging.info("Step 2: Resample hourly data to daily data")
-            ds = xr.open_mfdataset(self.files, parallel=False)
+            # Step 3: Resample hourly data to daily data using xarray
+            logging.info("Step 3: Resample hourly data to daily data")
             daily_ds = ds.resample(time='1D').mean().rename({'time': 'date'})
-
-            # Step 3: Create a directory to save independent variables
-            logging.info("Step 3: Make a directory to save independent variables")
-            output_dir = os.path.join(os.getcwd(), "independent-variables",self.variable)
-            os.makedirs(output_dir, exist_ok=True)
 
             # Step 4: Convert the resampled dataset to a Dask DataFrame
             logging.info("Step 4: Convert Dataset to DataFrame")
-            df = daily_ds.to_dask_dataframe().repartition(npartitions=10)
+            df = daily_ds.to_dask_dataframe().repartition(npartitions=240)
+
 
             # Step 5: Save the DataFrame as a parquet file in the output directory
-            name_function = lambda x: f"{self.variable}-{x}.parquet"
+            name_function = lambda x: f"weather_data{self.year}-{x}.parquet"
             df.to_parquet(output_dir,name_function=name_function)
-            logging.info("Step 5: Data Saved to independent-variables/")
+            logging.info(f"Step 5: {self.year} Data Saved to {output_dir}")
 
             # Step 6: Close the Dask Cluster
-            return cluster.close()
+            cluster.close()
 
+            return output_dir
+        
         except Exception as e:
-            # If any exception occurs, raise a CustomException
-            raise CustomException(e, sys)
+            raise CustomException(e,sys)
--- src/piplines/weather_pipline.py
+++ src/piplines/weather_pipline.py
@@ -1,52 +1,10 @@
-import threading
-import concurrent.futures
-
-from src.utils.climatology import create_cdsapirc_file
-from src.components.WeatherDataProcess import (WeatherDataConfig, 
-                                               WeatherDataDownloader, 
-                                               WeatherTransformer)
+from src.components.WeatherDataProcess import WeatherDataProcessor
 
 
-CDSAPI_KEY = "1004:751df99f-695b-494a-bb42-11652a943996"
 START_YEAR = 2016
 END_YEAR = 2022
 AREA_OF_INTEREST=[44.547420,6.795044,46.176027,12.084961]
-VARIABLES = ["2m_temperature","2m_dewpoint_temperature",
-             "10m_u_component_of_wind","10m_v_component_of_wind",
-             "total_precipitation","surface_net_solar_radiation"]
-MAX_CONCURRENT_DOWNLOADS = 10 # CDS does not let to download more than 10 simultaneously
-
-# Assign Key
-create_cdsapirc_file(key=CDSAPI_KEY)
-
-# Semaphore to control the number of concurrent downloads
-download_semaphore = threading.Semaphore(MAX_CONCURRENT_DOWNLOADS)
-
-def download_weather_data(variable, year, month):
-    config = WeatherDataConfig(year=year, month=month, area=AREA_OF_INTEREST, variable=[variable])
-    obj = WeatherDataDownloader(config)
-    
-    # Acquire semaphore before downloading
-    with download_semaphore:
-        file_path = obj.download()
-    
-    return file_path
-
-def parallel_download(variable):
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        futures = []
-        for year in range(START_YEAR, END_YEAR):
-            for month in range(1, 13, MAX_CONCURRENT_DOWNLOADS):
-                # Create a batch of months (up to MAX_CONCURRENT_DOWNLOADS)
-                batch = [(year, m) for m in range(month, min(month + MAX_CONCURRENT_DOWNLOADS, 13))]
-                futures.extend(executor.submit(download_weather_data, variable, year, m) for _, m in batch)
-
-        # Wait for all tasks to complete
-        results = [future.result() for future in futures]
-
-    return results
 
 if __name__ == "__main__":
-    for var in VARIABLES:
-        file_paths = parallel_download(var)
-        WeatherTransformer(file_paths,var).convertor()
+    for year in range(START_YEAR, END_YEAR+1):
+        OUTPUT = WeatherDataProcessor(year=year, area=AREA_OF_INTEREST).Transformer()
